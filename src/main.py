7c0022096c1486c4d27eb06a4247aef62eacc78b
Samy-Le-S <127564082+Samy-Le-S@users.noreply.github.com>
2025-02-28 12:28:04 +0100

    importation des images

--- src/main.py
+++ src/main.py
@@ -10,14 +10,17 @@ clock = pg.time.Clock()
 #Chargement des fichiers
 sourceFileDir=os.path.dirname(os.path.abspath(__file__))
 os.chdir(sourceFileDir)
-
 #ecran
 pg.display.set_caption("Basket Duel")
 fenetre = pg.display.set_mode((1280, 720))
 
 #importabtion des images
-
+ 
 terrain = Terrain()
+balle_position = (100, 100)
+
+def check_collision(balle_rect,panier_rect):
+    return balle_rect.colliderect(panier_rect)
 
 while True:
     for event in pg.event.get():
@@ -25,6 +28,15 @@ while True:
             pg.quit()
             sys.exit()
     terrain.afficherTerrain(fenetre)
+    terrain.afficherBalle(fenetre, balle_position)
+    
+    balle_rect = terrain.balle.get_rect(topleft=balle_position)
+    panier_rect = terrain.panier.get_rect(topleft=terrain.positionPanier)
+    
+    if check_collision(balle_rect, panier_rect):
+        terrain.positionPanier = terrain.genererPositionPanier() 
+    
+    terrain.afficherPanier(fenetre)
     pg.display.flip()
     clock.tick(60)
 
