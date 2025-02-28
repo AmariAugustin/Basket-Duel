bf7dfce707ff2223b0f3b43a7a39af8f6cdeb88f
Samy-Le-S <127564082+Samy-Le-S@users.noreply.github.com>
2025-02-28 15:16:27 +0100

    hitboxes, collissions

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import pygame as pg
 import sys
 from Joueur import *
 from Terrain import *
+from balle import Balle
 import os
 
 pg.init()
@@ -14,30 +15,56 @@ os.chdir(sourceFileDir)
 pg.display.set_caption("Basket Duel")
 fenetre = pg.display.set_mode((1280, 720))
 
-#importabtion des images
- 
 terrain = Terrain()
-balle_position = (100, 100)
+balle = Balle([100, 100], speed=2)
 
-def check_collision(balle_rect,panier_rect):
+def check_collision(balle_rect, panier_rect):
     return balle_rect.colliderect(panier_rect)
+
+def is_hitbox_within_terrain(hitbox, terrain_width, terrain_height):
+    return (hitbox.left >= 0 and hitbox.right <= terrain_width and
+            hitbox.top >= 0 and hitbox.bottom <= terrain_height)
+
+show_hitboxes = False
 
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             sys.exit()
-    terrain.afficherTerrain(fenetre)
-    terrain.afficherBalle(fenetre, balle_position)
+        balle.handle_event(event)
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_j:
+                show_hitboxes = not show_hitboxes
     
-    balle_rect = terrain.balle.get_rect(topleft=balle_position)
-    panier_rect = terrain.panier.get_rect(topleft=terrain.positionPanier)
+    terrain.afficherTerrain(fenetre)
+    balle.update_position(fenetre.get_width(), fenetre.get_height())
+    balle.draw(fenetre)
+    
+    # possibilité de montrer les hitbox
+    if show_hitboxes:
+        balle_rect = balle.rect
+        panier_rect_full = terrain.panier.get_rect(topleft=terrain.positionPanier)
+        # modification de l'hitbox panier
+        hitbox_height = 50
+        panier_rect = pg.Rect(panier_rect_full.left, panier_rect_full.top + 10, panier_rect_full.width, hitbox_height)
+        
+        # si panier pas dans le terrain, le replace
+        if not is_hitbox_within_terrain(panier_rect, terrain.largeur, terrain.hauteur):
+            terrain.positionPanier = terrain.genererPositionPanier()
+        #affichage hitbox
+        pg.draw.rect(fenetre, (255, 0, 0), balle_rect, 2)  
+        pg.draw.rect(fenetre, (0, 255, 0), panier_rect, 2)  
+    
+    # check les collisions
+    balle_rect = balle.rect
+    panier_rect_full = terrain.panier.get_rect(topleft=terrain.positionPanier)
+    hitbox_height = 50
+    panier_rect = pg.Rect(panier_rect_full.left, panier_rect_full.top + 10, panier_rect_full.width, hitbox_height)
     
     if check_collision(balle_rect, panier_rect):
-        terrain.positionPanier = terrain.genererPositionPanier() 
+        terrain.positionPanier = terrain.genererPositionPanier()
     
     terrain.afficherPanier(fenetre)
     pg.display.flip()
     clock.tick(60)
-
-
