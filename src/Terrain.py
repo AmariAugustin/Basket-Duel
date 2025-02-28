0c9bffedff873de8d625b0b0510b1921bc9bffe1
Samy-Le-S <127564082+Samy-Le-S@users.noreply.github.com>
2025-02-28 12:27:13 +0100

    ajout des collisions, panier et balle

--- src/Terrain.py
+++ src/Terrain.py
@@ -10,18 +10,27 @@ class Terrain:
     def __init__(self):  
         self.largeur = 1000
         self.hauteur = 1000
-        self.positionPanier = None  
+        self.positionPanier = self.genererPositionPanier()  
         self.terrainAsset = pg.image.load("assets/terrain.png")
-
+        self.balle = pg.image.load("assets/basketball.png") 
+        self.balle = pg.transform.scale(self.balle, (50, 50))
+        self.panier = pg.image.load("assets/hoop.png")
+        self.panier = pg.transform.scale(self.panier, (50,50))
+        
     def genererPositionPanier(self):  
-        self.positionPanier = [random.randint(0, 200)]
-        return self.positionPanier
+        return [random.randint(0, self.largeur - 100), random.randint(0, self.hauteur - 100)]
     
     # def genererPositionJoueur(self):
 
     def afficherTerrain(self,fenetre):
         fenetre.blit(self.terrainAsset, (0, 0))
 
+    def afficherBalle(self, fenetre, position):
+        fenetre.blit(self.balle, position)
+        
+    def afficherPanier(self, fenetre):
+        position = self.genererPositionPanier()
+        fenetre.blit(self.panier, position)
 # Test
 terrain = Terrain()
-print(terrain.genererPositionPanier())  
+print(terrain.genererPositionPanier())
