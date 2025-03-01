fd7a65b9da537bec9030eeede223c59704ed8fe4
Samy-Le-S <127564082+Samy-Le-S@users.noreply.github.com>
2025-03-01 14:44:02 +0100

    création de la classe joueur

---
+++ src/Joueur.py
@@ -0,0 +1,17 @@
+import pygame as pg
+import random
+
+class Joueur:
+    def __init__(self, position=None):#affiche joueur
+        self.joueur_image = pg.transform.scale(pg.image.load("assets/player.png"), (100, 100))
+        self.rect = self.joueur_image.get_rect()
+        if position is None:
+            position = self.genererPositionJoueur()
+        self.position = position
+        self.rect.topleft = self.position
+
+    def genererPositionJoueur(self): #crée position joueur (comme panier)
+        return [random.randint(0, 1280 - self.rect.width), random.randint(0, 720 - self.rect.height)]
+
+    def draw(self, fenetre): #dessine joueur
+        fenetre.blit(self.joueur_image, self.position)
