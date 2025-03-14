378e3d1befa0e84234352665c95c482fbfdff508
Augustin Amari <amariaugustin4@gmail.com>
2025-03-14 06:23:27 +0100

    remove a test

--- src/serveur.py
+++ src/serveur.py
@@ -22,24 +22,6 @@ class Serveur:
     def close(self):
         self.clientsocket.close()
         self.s.close()
-
-    def controleBalle(self, balle, joueur, msg):
-        if (msg == "droite"):
-            joueur.position[0] += 10
-        elif (msg == "gauche"):
-            joueur.position[0] -= 10
-        elif (msg == "haut"):
-            joueur.position[1] -= 10
-        elif (msg == "bas"):
-            joueur.position[1] += 10
-        elif (msg == "tir"):   
-            balle.shooting_mode = False
-            balle.flying = True
-            balle.velocity_x = 10
-            balle.velocity_y = 10
-            balle.position[0] = joueur.position[0]
-            balle.position[1] = joueur.position[1] - 30
-            balle.rect.topleft = balle.position
   
 s = Serveur()
 
